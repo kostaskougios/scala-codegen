2dfbbe3a2fe8cd6c824af342220bf31c6f386366
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2017-08-21 16:50:08 +0100

    enhancer

--- macros/src/main/scala/com/aktit/macros/Enhancer.scala
+++ macros/src/main/scala/com/aktit/macros/Enhancer.scala
@@ -16,7 +16,11 @@ class Enhancer(files: Seq[File])
     val source = scala.io.Source.fromFile(file, "UTF-8").mkString
     source.parse[Source] match {
       case Parsed.Success(tree) =>
-        println(tree)
+        tree match {
+          case q"trait $traitName [..$tparams] extends ..$t { $body }" =>
+            println(s"A trait declaration with name $traitName")
+          case _ => throw new IllegalArgumentException(s"can't recognize $tree")
+        }
       case Parsed.Error(_, _, details) =>
         throw details
     }
