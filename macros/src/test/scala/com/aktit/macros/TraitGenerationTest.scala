52d7fea036bcdf484cc192496c1b4df7e983f0bf
kostas.kougios <kostas.kougios@googlemail.com>
2017-09-29 08:18:41 +0100

    code generation

--- macros/src/test/scala/com/aktit/macros/TraitGenerationTest.scala
+++ macros/src/test/scala/com/aktit/macros/TraitGenerationTest.scala
@@ -15,4 +15,17 @@ class TraitGenerationTest extends AbstractSuite
 		val tm = t.withMethods(t.methods.map(_.withName("ff")))
 		tm.syntax should be(q"trait x { def ff(i:Int): String }".syntax)
 	}
+
+	test("copy a method") {
+		val t = Trait.parser(q"trait x { def f(i:Int): String }")
+		val toCopy = t.methods.head
+		val tm = t.withMethods(t.methods :+ toCopy)
+		tm.syntax should be(
+			q"""
+			   trait x {
+			   	def f(i:Int): String
+			   	def f(i:Int): String
+			   	}
+				""".syntax)
+	}
 }
