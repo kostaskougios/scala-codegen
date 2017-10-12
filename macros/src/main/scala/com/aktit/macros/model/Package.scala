4e3ae297edd5a45900e77b41434a4965072545df
kostas.kougios <kostas.kougios@googlemail.com>
2017-10-12 17:45:56 +0100

    meta refactoring

--- macros/src/main/scala/com/aktit/macros/model/Package.scala
+++ macros/src/main/scala/com/aktit/macros/model/Package.scala
@@ -6,13 +6,16 @@ import scala.meta._
   * @author kostas.kougios
   *         Date: 29/08/17
   */
-case class Package(tree: Tree, nameTerm: Term.Ref, children: Seq[N]) extends N
+case class Package(meta: Package.Meta, children: Seq[N]) extends N
+	with Meta.Contains[Package.Meta]
 	with Code
 	with Code.Name[Package]
 {
-	def name: String = nameTerm.syntax
+	def name: String = meta.nameTerm.syntax
 
-	override def withName(name: String) = copy(nameTerm = Term.Name(name))
+	override def tree = meta.tree
+
+	override def withName(name: String) = copy(meta = meta.copy(nameTerm = Term.Name(name)))
 
 	def traits: Seq[Trait] = children.collect {
 		case t: Trait => t
@@ -27,11 +30,15 @@ object Package extends PartialParser[Package]
 	override def parser = {
 		case tree@q"package $ref { ..$topstats }" =>
 			Package(
-				tree,
-				ref,
+				Meta(
+					tree,
+					ref),
 				topstats.map(
 					Trait.parser.orElse(Class.parser)
 				)
 			)
 	}
+
+	case class Meta(tree: Tree, nameTerm: Term.Ref) extends com.aktit.macros.model.Meta
+
 }
