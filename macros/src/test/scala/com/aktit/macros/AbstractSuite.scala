64f7f8e78c2b2cdf622c2e7d0e9730c23a48ca38
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2017-11-02 11:51:29 +0000

    cleanup

--- macros/src/test/scala/com/aktit/macros/AbstractSuite.scala
+++ macros/src/test/scala/com/aktit/macros/AbstractSuite.scala
@@ -1,21 +1,9 @@
 package com.aktit.macros
 
-import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.{ FunSuite, Matchers }
 
 /**
   * @author kostas.kougios
   *         Date: 01/09/17
   */
 abstract class AbstractSuite extends FunSuite with Matchers
-{
-	//	protected def codeDirectory: File = {
-	//		val f = new File("code")
-	//		if (f.exists)
-	//			f
-	//		else new File(".")
-	//	}
-	//
-	//	protected def codeSourceFolder = SourceFolder(new File(codeDirectory, "src/main/scala"))
-	//
-	//	protected def fileParse(fullClassName: String) = codeSourceFolder.parse(fullClassName)
-}
