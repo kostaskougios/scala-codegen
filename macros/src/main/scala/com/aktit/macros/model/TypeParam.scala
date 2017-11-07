4b4923bc895b90a9d36c80de54aadb9da81cd5f4
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2017-11-07 16:48:38 +0000

    type param to type

--- macros/src/main/scala/com/aktit/macros/model/TypeParam.scala
+++ macros/src/main/scala/com/aktit/macros/model/TypeParam.scala
@@ -2,15 +2,21 @@ package com.aktit.macros.model
 
 import com.aktit.macros.model
 
+import scala.meta._
+
 /**
   * @author kostas.kougios
   *         Date: 03/11/17
   */
 case class TypeParam(meta: TypeParam.Meta) extends Code with Meta.Contains
 {
-  def toType = Type(
-    t"${meta.param.name}[..${meta.param.tparams}]"
-  )
+  def toType = {
+    val typeArgs = meta.param.tparams.map(_.name.value)
+    val ta = if (typeArgs.isEmpty) "" else s"[${typeArgs.mkString(",")}]"
+    Type(
+      s"${meta.param.name}".parse[scala.meta.Type].get
+    )
+  }
 
   override def tree = meta.param
 }
