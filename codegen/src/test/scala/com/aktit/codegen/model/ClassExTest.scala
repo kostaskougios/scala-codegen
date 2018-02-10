766913d276a7f88e9aa23afea2d7d9c5dbb9b5d4
kostas.kougios@googlemail.com <kostas.kougios@googlemail.com>
2018-02-10 11:00:47 +0000

    tests

--- codegen/src/test/scala/com/aktit/codegen/model/ClassExTest.scala
+++ codegen/src/test/scala/com/aktit/codegen/model/ClassExTest.scala
@@ -10,21 +10,33 @@ import scala.meta._
   */
 class ClassExTest extends AbstractSuite
 {
-    test("withName") {
-        val t = ClassEx.parser(q"class x(i:Int) { def f: String = i.toString }")
-        val tm = t.withName("y")
-        tm.syntax should be(q"class y(i:Int) { def f: String = i.toString }".syntax)
-    }
+	test("withName") {
+		val t = ClassEx.parser(q"class x(i:Int) { def f: String = i.toString }")
+		val tm = t.withName("y")
+		tm.syntax should be(q"class y(i:Int) { def f: String = i.toString }".syntax)
+	}
 
-    test("withMethods") {
-        val t = ClassEx.parser(q"class x(i:Int) { def f: String = i.toString }")
-        val tm = t.withMethods(t.methods.map(_.withName("ff")))
-        tm.syntax should be(q"class x(i:Int) { def ff: String = i.toString }".syntax)
-    }
+	test("withMethods") {
+		val t = ClassEx.parser(q"class x(i:Int) { def f: String = i.toString }")
+		val tm = t.withMethods(t.methods.map(_.withName("ff")))
+		tm.syntax should be(q"class x(i:Int) { def ff: String = i.toString }".syntax)
+	}
 
-    test("vals") {
-        val c = ClassEx.parser(
-            q"""
+	test("methods") {
+		val t = ClassEx.parser(
+			q"""
+               class X(val i:Int) {
+                def f: String = i.toString
+				val a:String
+                def g: String = i.toString
+                }
+            """)
+		t.methods.map(_.name) should be(Seq("f", "g"))
+	}
+
+	test("vals") {
+		val c = ClassEx.parser(
+			q"""
            class X(val constrArg:Int){
                private val privVal:Int = 5
                protected val protVal:Int=8
@@ -32,48 +44,48 @@ class ClassExTest extends AbstractSuite
                def ignore:Long=10
            }
      """)
-        c.vals.map(_.name) should be(Seq("constrArg", "privVal", "protVal", "pubVal"))
-    }
+		c.vals.map(_.name) should be(Seq("constrArg", "privVal", "protVal", "pubVal"))
+	}
 
-    test("isCaseClass positive") {
-        ClassEx.parser(q"case class X(constrArg:Int)").isCaseClass should be(true)
-    }
+	test("isCaseClass positive") {
+		ClassEx.parser(q"case class X(constrArg:Int)").isCaseClass should be(true)
+	}
 
-    test("isCaseClass negative") {
-        ClassEx.parser(q"class X").isCaseClass should be(false)
-    }
+	test("isCaseClass negative") {
+		ClassEx.parser(q"class X").isCaseClass should be(false)
+	}
 
-    test("constructor arg without modifiers") {
-        val c = ClassEx.parser(q"class X(constrArg:Int)")
-        c.vals should be(Nil)
-    }
+	test("constructor arg without modifiers") {
+		val c = ClassEx.parser(q"class X(constrArg:Int)")
+		c.vals should be(Nil)
+	}
 
-    test("constructor public arg") {
-        val c = ClassEx.parser(q"class X(val publ:Int)")
-        c.vals.map(_.name) should be(Seq("publ"))
-        c.vals.head.isPublic should be(true)
-    }
+	test("constructor public arg") {
+		val c = ClassEx.parser(q"class X(val publ:Int)")
+		c.vals.map(_.name) should be(Seq("publ"))
+		c.vals.head.isPublic should be(true)
+	}
 
-    test("constructor private arg") {
-        val c = ClassEx.parser(q"class X(private val priv:Int)")
-        c.vals.map(_.name) should be(Seq("priv"))
-        c.vals.head.isPrivate should be(true)
-    }
+	test("constructor private arg") {
+		val c = ClassEx.parser(q"class X(private val priv:Int)")
+		c.vals.map(_.name) should be(Seq("priv"))
+		c.vals.head.isPrivate should be(true)
+	}
 
-    test("constructor arg a private val") {
-        val c = ClassEx.parser(q"class X(private val constrArg:Int)")
-        c.vals.map(_.name) should be(Seq("constrArg"))
-    }
+	test("constructor arg a private val") {
+		val c = ClassEx.parser(q"class X(private val constrArg:Int)")
+		c.vals.map(_.name) should be(Seq("constrArg"))
+	}
 
-    test("case class vals") {
-        val c = ClassEx.parser(q"case class X(constrArg:Int)")
-        c.vals.map(_.name) should be(Seq("constrArg"))
-        c.vals.count(_.isPublic) should be(1)
-    }
+	test("case class vals") {
+		val c = ClassEx.parser(q"case class X(constrArg:Int)")
+		c.vals.map(_.name) should be(Seq("constrArg"))
+		c.vals.count(_.isPublic) should be(1)
+	}
 
-    test("case class private vals") {
-        val c = ClassEx.parser(q"case class X(private constrArg:Int)")
-        c.vals.map(_.name) should be(Seq("constrArg"))
-        c.vals.count(_.isPrivate) should be(1)
-    }
+	test("case class private vals") {
+		val c = ClassEx.parser(q"case class X(private constrArg:Int)")
+		c.vals.map(_.name) should be(Seq("constrArg"))
+		c.vals.count(_.isPrivate) should be(1)
+	}
 }
