6816af44d99479ba2c344f22d83a03968ccf0b58
kostas.kougios@googlemail.com <kostas.kougios@googlemail.com>
2018-02-13 21:32:57 +0000

    imports

--- codegen/src/test/scala/com/aktit/codegen/model/ImportExTest.scala
+++ codegen/src/test/scala/com/aktit/codegen/model/ImportExTest.scala
@@ -6,6 +6,11 @@ import scala.meta._
 
 class ImportExTest extends AbstractSuite
 {
+	test("import") {
+		val imports = ImportEx.parser(q"import com.pcg1.A")
+		imports.imports should be(Seq(Imported("com.pcg1", "A")))
+	}
+
 	test("multiple imports") {
 		val imports = ImportEx.parser(q"import com.pcg1.{A,B}")
 		imports.imports should be(Seq(Imported("com.pcg1", "A"), Imported("com.pcg1", "B")))
