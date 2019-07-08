213e851345e396b1dba347d40d9e36d93d9a0cee
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2019-07-08 23:21:11 +0100

    ObjectEx

---
+++ codegen/src/main/scala/com/aktit/codegen/model/ObjectEx.scala
@@ -0,0 +1,32 @@
+package com.aktit.codegen.model
+
+import scala.meta._
+
+/**
+  * @author kostas.kougios
+  *         08/07/19 - 23:14
+  */
+case class ObjectEx(meta: ObjectEx.Meta)
+	extends CodeEx
+		with MethodEx.Contains[ObjectEx]
+{
+	override def tree = q"..${meta.mods.toList} object ${meta.ename} extends ${meta.template}"
+
+	override protected def withTemplateInner(t: Template) = copy(meta = meta.copy(template = t))
+}
+
+object ObjectEx extends PartialParser[ObjectEx]
+{
+
+	case class Meta(
+		mods: Seq[Mod],
+		ename: Term.Name,
+		template: Template
+	) extends MetaEx with MetaEx.Template
+
+	override def parser = {
+		case q"..$mods object $ename extends $template" =>
+			ObjectEx(Meta(mods, ename, template))
+	}
+
+}
