ebcc6ad83d049ad1acdf117548fcbc90b9cf0427
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2019-07-08 23:23:38 +0100

    ObjectEx

--- codegen/src/main/scala/com/aktit/codegen/model/ObjectEx.scala
+++ codegen/src/main/scala/com/aktit/codegen/model/ObjectEx.scala
@@ -6,13 +6,18 @@ import scala.meta._
   * @author kostas.kougios
   *         08/07/19 - 23:14
   */
-case class ObjectEx(meta: ObjectEx.Meta)
-	extends CodeEx
-		with MethodEx.Contains[ObjectEx]
+case class ObjectEx(meta: ObjectEx.Meta) extends CodeEx
+	with MethodEx.Contains[ObjectEx]
+	with MetaEx.Contains
+	with MetaEx.ContainsMods[ObjectEx]
 {
 	override def tree = q"..${meta.mods.toList} object ${meta.ename} extends ${meta.template}"
 
 	override protected def withTemplateInner(t: Template) = copy(meta = meta.copy(template = t))
+
+	override def withMods(mods: ModsEx) = copy(
+		meta = meta.copy(mods = mods.meta.mods)
+	)
 }
 
 object ObjectEx extends PartialParser[ObjectEx]
@@ -22,7 +27,7 @@ object ObjectEx extends PartialParser[ObjectEx]
 		mods: Seq[Mod],
 		ename: Term.Name,
 		template: Template
-	) extends MetaEx with MetaEx.Template
+	) extends MetaEx with MetaEx.Template with MetaEx.Mods
 
 	override def parser = {
 		case q"..$mods object $ename extends $template" =>
