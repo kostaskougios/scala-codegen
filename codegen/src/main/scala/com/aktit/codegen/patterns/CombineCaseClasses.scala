d889ef47dce069b49e984af1b5aff4ba55df7fdf
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2019-07-08 20:45:35 +0100

    combine case classes

--- codegen/src/main/scala/com/aktit/codegen/patterns/CombineCaseClasses.scala
+++ codegen/src/main/scala/com/aktit/codegen/patterns/CombineCaseClasses.scala
@@ -1,6 +1,6 @@
 package com.aktit.codegen.patterns
 
-import com.aktit.codegen.model.{ClassEx, ModsEx}
+import com.aktit.codegen.model.{ClassEx, ModsEx, PackageEx}
 
 /**
   * @author kostas.kougios
@@ -8,11 +8,14 @@ import com.aktit.codegen.model.{ClassEx, ModsEx}
   */
 object CombineCaseClasses
 {
-	def combine(newClassName: String, classes: ClassEx*) = {
+	def combine(targetPackage: String, newClassName: String, classes: ClassEx*) = {
 		val vals = classes.flatMap(_.vals)
 
-		ClassEx.withName(newClassName)
+		val caseClass = ClassEx.withName(newClassName)
 			.withConstructorParameters(vals.map(_.toTermParamEx.withMods(ModsEx.empty)))
 			.withCaseClass
+
+		PackageEx.withName(targetPackage)
+			.withClasses(Seq(caseClass))
 	}
 }
--- codegen/src/test/scala/com/aktit/codegen/patterns/CombineCaseClassesTest.scala
+++ codegen/src/test/scala/com/aktit/codegen/patterns/CombineCaseClassesTest.scala
@@ -10,21 +10,51 @@ import org.scalatest.Matchers._
   */
 class CombineCaseClassesTest extends FunSuite
 {
+	val itemPackage = PackageEx.fromSource(
+		"""
+		  |package x1
+		  |
+		  |case class Item(id:Int,name:String)
+		""".stripMargin)
+
+	val basketPackage = PackageEx.fromSource(
+		"""
+		  |package x2
+		  |
+		  |case class Basket(discount:Float,numOfItems:Int)
+		""".stripMargin)
+
 	test("combines case classes") {
-		val itemPackage = PackageEx.fromSource(
+		val combined = CombineCaseClasses.combine("tx", "BasketedItem", itemPackage.classes ++ basketPackage.classes: _*)
+		combined.syntax should be(PackageEx.fromSource(
+			"""
+			  |package tx
+			  |
+			  |case class BasketedItem(id: Int, name: String, discount: Float, numOfItems: Int)
+			  |""".stripMargin
+		).syntax
+		)
+	}
+
+	test("import dependencies") {
+		val itemWithDate = PackageEx.fromSource(
 			"""
 			  |package x1
 			  |
-			  |case class Item(id:Int,name:String)
-			""".stripMargin)
-
-		val basketPackage = PackageEx.fromSource(
-			"""
-			  |package x2
+			  |import java.sql.Date
 			  |
-			  |case class Basket(discount:Float,numOfItems:Int)
+			  |case class Item(id:Int,date:Date)
 			""".stripMargin)
-		val combined = CombineCaseClasses.combine("BasketedItem", itemPackage.classes ++ basketPackage.classes: _*)
-		combined.syntax should be("case class BasketedItem(id: Int, name: String, discount: Float, numOfItems: Int)")
+		val combined = CombineCaseClasses.combine("tx", "BasketedItem", itemWithDate.classes ++ basketPackage.classes: _*)
+		combined.syntax should be(PackageEx.fromSource(
+			"""
+			  |package tx
+			  |
+			  |import java.sql.Date
+			  |
+			  |case class BasketedItem(id: Int, date:Date, discount: Float, numOfItems: Int)
+			""".stripMargin).syntax
+		)
+
 	}
 }
