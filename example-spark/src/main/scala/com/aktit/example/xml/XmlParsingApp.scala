6d9e91ce8b1e1b84fdbf8feba5358dc10cd2a8ef
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2019-08-23 22:26:55 +0100

    xml spark

--- example-spark/src/main/scala/com/aktit/example/xml/XmlParsingApp.scala
+++ example-spark/src/main/scala/com/aktit/example/xml/XmlParsingApp.scala
@@ -11,11 +11,22 @@ object XmlParsingApp extends App
 {
 	val spark = new SparkSession.Builder().master("local[*]").getOrCreate
 
+
 	try {
+		import spark.implicits._
 		val df = spark.read
 			.option("rowTag", "user")
 			.xml("xml-files/test.xml")
 
-		df.show()
+		df.show(false)
+		df.printSchema()
+
+		println(df.as[User].collect.mkString("\n"))
 	} finally spark.close()
 }
+
+case class User(name: String, country: String, projects: Projects)
+
+case class Projects(project: Seq[Project])
+
+case class Project(name: String)
