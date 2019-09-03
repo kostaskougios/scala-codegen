c484ca0421c867efd3caa8e2727af361143d4393
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2019-09-03 11:33:38 +0100

    xml to case class impl

--- codegen-spark/src/main/scala/com/aktit/codegen/spark/XmlToCaseClass.scala
+++ codegen-spark/src/main/scala/com/aktit/codegen/spark/XmlToCaseClass.scala
@@ -10,32 +10,36 @@ private class XmlToCaseClass(
 	targetPackage: String,
 	newClassName: String,
 	xml: Elem,
-	elementToVariableName: String => String
+	elementToVariableName: String => String,
+	elementToClassName: String => String
 )
 {
-	def build = {
-		val allClasses = xml.child.collect {
-			case e: Elem => deepScan(e)
-		}.collect {
-			case c: ScannedClass => c.classes
-		}.flatten
-		allClasses.groupBy(_.name).map {
+	def build = resultingClasses
+
+	def resultingClasses = scannedClasses.groupBy(_.name)
+		.map {
 			case (n, cl) =>
 				ResultingClass(
-					n,
+					elementToClassName(n),
 					cl.flatMap(_.children)
 						.groupBy(_.name)
 						.map {
-							case (_, s) => s.head match {
-								case ScannedField(field) =>
-									ResultingField(elementToVariableName(field), "String")
-								case ScannedClass(clz, _) =>
-									ResultingField(elementToVariableName(clz), clz)
-							}
+							case (_, s) =>
+								s.head match {
+									case ScannedField(field) =>
+										ResultingField(elementToVariableName(field), "String")
+									case ScannedClass(clz, _) =>
+										ResultingField(elementToVariableName(clz), elementToClassName(clz))
+								}
 						}.toList
 				)
 		}
-	}
+
+	private def scannedClasses = xml.child.collect {
+		case e: Elem => deepScan(e)
+	}.collect {
+		case c: ScannedClass => c.classes
+	}.flatten
 
 	private def deepScan(n: Elem): Scanned = {
 		val elems = n.child.collect {
@@ -72,9 +76,10 @@ object XmlToCaseClass
 		targetPackage: String,
 		newClassName: String,
 		xmlFile: String,
-		headerToVariableName: String => String = ColumnNameToFieldName.defaultColumnNameToFieldName
+		headerToVariableName: String => String = ColumnNameToFieldName.defaultColumnNameToFieldName,
+		elementToClassName: String => String = ColumnNameToFieldName.defaultColumnNameToFieldName
 	) = {
 		val xml = XML.loadFile(xmlFile)
-		new XmlToCaseClass(targetPackage, newClassName, xml, headerToVariableName).build
+		new XmlToCaseClass(targetPackage, newClassName, xml, headerToVariableName, elementToClassName).build
 	}
 }
