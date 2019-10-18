317ab2c9c91ecbdc36e09beec1fea21e1c36696e
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2019-10-18 18:06:16 +0100

    spark -> codegen

--- codegen-spark/src/main/scala/com/aktit/codegen/spark/Naming.scala
+++ codegen-spark/src/main/scala/com/aktit/codegen/spark/Naming.scala
@@ -7,15 +7,20 @@ import org.apache.spark.sql.types.StructField
   * @author kostas.kougios
   *         23/08/2019 - 22:05
   */
-object ColumnToFieldName
+object Naming
 {
 	private val Replace = Array("\"", "(", ")", "-", "+", "_", "/", "?")
 	private val ReplaceWith = Array("", "", "", "", "", "", "", "")
 
-	def defaultColumnNameToFieldName(field: StructField): String = {
+	def defaultFieldToFieldName(field: StructField): String = {
 		val f = StringUtils.replaceEach(field.name, Replace, ReplaceWith)
 		val a = StringUtils.split(f, ' ')
 		(StringUtils.uncapitalize(a(0)) +: a.tail.map(StringUtils.capitalize)).mkString
 	}
 
+	def defaultFieldToClassName(field: StructField): String = {
+		val f = StringUtils.replaceEach(field.name, Replace, ReplaceWith)
+		val a = StringUtils.split(f, ' ')
+		(StringUtils.capitalize(a(0)) +: a.tail.map(StringUtils.capitalize)).mkString
+	}
 }
--- codegen-spark/src/main/scala/com/aktit/codegen/spark/SparkSchemaToClasses.scala
+++ codegen-spark/src/main/scala/com/aktit/codegen/spark/SparkSchemaToClasses.scala
@@ -9,30 +9,53 @@ import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
   */
 class SparkSchemaToClasses private(
 	targetPackage: String,
-	fieldToFieldName: StructField => String
+	fieldToFieldName: StructField => String,
+	fieldToClassName: StructField => String
 )
 {
 	def toPackage(topClassName: String, schema: StructType) =
 		PackageEx.withName(targetPackage)
 			.withClass(toClass(topClassName, schema))
 
-	def toClass(className: String, schema: StructType) =
+	def toClass(className: String, schema: StructType): ClassEx = {
+		val scanned = toConstructorParams(schema.fields)
 		ClassEx.withName(className)
 			.withConstructorParameters(
-				toConstructorParams(schema.fields)
+				scanned.map(_.toParam)
 			).withCaseClass
+	}
 
-	def toConstructorParams(fields: Seq[StructField]) = fields.map {
+	private def toConstructorParams(fields: Seq[StructField]): Seq[Scanned] = fields.map {
 		field =>
 			val fieldName = fieldToFieldName(field)
-			val tpe = field.dataType match {
+			field.dataType match {
 				case DataTypes.BooleanType =>
-					"Boolean"
-				//				case DataTypes.StructType(fields) =>
+					Type(fieldName, "Boolean", field)
+				case s: StructType =>
+					Class(fieldName, toClass(fieldToClassName(field), s), field)
 
 			}
-			val t = if (field.nullable) s"Option[$tpe]" else tpe
-			TermParamEx.fromSource(s"$fieldName : $t")
+	}
+}
+
+private trait Scanned
+{
+	def toParam: TermParamEx
+}
+
+private case class Type(fieldName: String, name: String, field: StructField) extends Scanned
+{
+	override def toParam = {
+		val t = if (field.nullable) s"Option[${name}]" else name
+		TermParamEx.fromSource(s"$fieldName : $t")
+	}
+}
+
+private case class Class(fieldName: String, c: ClassEx, field: StructField) extends Scanned
+{
+	override def toParam = {
+		val t = if (field.nullable) s"Option[${c.name}]" else c.name
+		TermParamEx.fromSource(s"$fieldName : $t")
 	}
 }
 
@@ -42,7 +65,8 @@ object SparkSchemaToClasses
 		targetPackage: String,
 		topClassName: String,
 		schema: StructType,
-		fieldToFieldName: StructField => String = ColumnToFieldName.defaultColumnNameToFieldName
-	) = new SparkSchemaToClasses(targetPackage, fieldToFieldName)
+		fieldToFieldName: StructField => String = Naming.defaultFieldToFieldName,
+		fieldToClassName: StructField => String = Naming.defaultFieldToClassName
+	) = new SparkSchemaToClasses(targetPackage, fieldToFieldName, fieldToClassName)
 		.toPackage(topClassName, schema)
 }
